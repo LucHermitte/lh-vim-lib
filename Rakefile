9acb6fa0823299b5d49a0f9f9d13fc4a80982a8e
Luc Hermitte <luc.hermitte@gmail.com>
2024-08-18 05:18:55 +0200

    Clean specs

--- Rakefile
+++ Rakefile
@@ -14,11 +14,9 @@ end
 
 task :test    => :spec
 
-task :spec do
-  # sh "bundle exec rspec ~/.vim-flavor/repos/LucHermitte_vim-UT/spec/UT_spec_v2.rb"
-  sh "bundle exec rspec spec/UT_spec.rb"
-  # sh "bundle exec rspec ~/.vim-flavor/repos/LucHermitte_vim-UT/spec"
-end
+# task :spec do
+#   sh "bundle exec rspec spec/UT_spec.rb"
+# end
 
 task :install do
   sh 'cd tests && bundle exec vim-flavor install'
--- spec/UT_spec.rb
+++ spec/UT_spec.rb
@@ -56,31 +56,6 @@ RSpec.describe "unit tests" do
           end
         end
         expect(ok).to be_truthy, "expected test to succeed, got\n#{log}\n"
-        ### log_file = '/tmp/'+ file + '.log'
-        ##vim.command('call writefile(["test"], "'+log_file+'")')
-        ##vim.command('call lh#log#set_logger("file", "'+log_file+'")')
-        ##vim.command('call lh#log#this("Logging UT '+file+'")')
-        ### print "Check log file '#{log_file}' exists\n"
-        ##expect(log_file).to be_an_existing_file
-        ##result = vim.echo('lh#UT#check(0, "'+abs_file+'")')
-        ### pp "result: #{abs_file} -> #{result}"
-        ### Keep only the list =>
-        ##if not (result.nil? or result.empty?)
-        ##    # Clean echoed messages
-        ##    result = eval(result.match(/\[\d,.*\]\]/)[0])
-        ##end
-        ### pp "result0: #{result[0]}"
-        ##if result.nil? or result.empty? or (result[0] == 0)
-        ##    print "Log file: #{file}.log"
-        ##    if File.file?(log_file)
-        ##        log = File.read(log_file)
-        ##        print "LOG: #{log}\n"
-        ##    else
-        ##        print "Warning: Cannot read #{log_file}\n"
-        ##    end
-        ##end
-        ##expect(result).to_not be_nil
-        ##expect(result).to be_successful
       end
     }
   end
